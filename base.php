d40647d46853dbf468bc160b8fe21d61f493e727
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-12-23 18:54:59 +0100

    Correct sql code IN DBWEBSITE.txt

--- base.php
+++ base.php
@@ -120,9 +120,11 @@
             </footer>
         </div>
 
-        <!-- jQuery, Popper.js and Bootstrap JS -->
+        <!-- jQuery and Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+        
+        <!-- Personal javascript files-->
         <script src="js/login_functions.js"></script>
     </body>
 </html>
