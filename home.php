b67e598728fc68df8c57b5c221e5d86e1a0c938d
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-12-28 17:56:00 +0100

    Fix error

--- home.php
+++ home.php
@@ -3,7 +3,7 @@ require_once 'boot.php';
 
 $mostSoldLimit = 8;
 $templateParams["file"] = TEMPLATE_DIR."template_home.php";
-$product = $dbh -> mostSold($mostSoldLimit);
+$product = $dbh->mostSold($mostSoldLimit);
 
 require 'base.php';
 ?>
