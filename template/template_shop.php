06f50ba3b0f0e30fec06f8a346e64eeb7650904b
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-12-28 17:55:04 +0100

    Add products list view in shop

--- template/template_shop.php
+++ template/template_shop.php
@@ -1,28 +1,54 @@
 <div class="row hidden-xs">
   <ul class="nav nav-pills nav-justified shop-nav">
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#hardware" aria-expanded="false" aria-controls="hardware">
-      Hardware
-    </button>
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#software" aria-expanded="false" aria-controls="software">
-      Software
-    </button>
-    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#gaming" aria-expanded="false" aria-controls="gaming">
-      Gaming
-    </button>
-    <div class="collapse collapse-category" id="hardware">
-      <div class="well">
-        <a href="#">Laptop</a>
-      </div>
+    <div class="row">
+      <?php foreach($templateParams["macrocategories"] as $macrocategory) : ?>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#<?php echo $macrocategory["name"];?>" aria-expanded="false" aria-controls="<?php echo $macrocategory["name"];?>">
+        <?php echo $macrocategory["name"]; ?>
+        </button> 
+      <?php endforeach; ?>
     </div>
-    <div class="collapse collapse-category" id="software">
-      <div class="well">
-        categorie software
+    <?php foreach($templateParams["macrocategories"] as $macrocategory) : ?>
+      <?php $macro = $macrocategory["id"];
+      $templateParams["categories"] = $dbh->getCategoriesByMacro($macro);
+      ?>
+      <div class="row">
+        <div class="collapse collapse-category" id="<?php echo $macrocategory["name"]; ?>">
+          <div class="well">
+          <h3><?php echo $macrocategory["name"]; ?></h4>
+          <?php foreach($templateParams["categories"] as $category): ?>
+            <a href="shop.php?macrocategoryid=<?php echo $macrocategory["id"]?>&categoryid=<?php echo $category["id"] ;?>">
+              <?php echo $category["name"]?>
+            </a>
+          <?php endforeach; ?>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="collapse collapse-category" id="gaming">
-      <div class="well">
-        gategorie gaming
-      </div>
-    </div>
+    <?php endforeach; ?>
   </ul>
 </div>
+
+<?php for($i = 0; $i < 2; $i = $i + 3):?>
+<div class="row text-center">
+    <div class="col-xs-12 col-sm-4 products-align">
+      <div class="products-align">
+        <img src="<?php echo "img/home_images/".$product[$i]["urlimage"] ?>" alt="home_image">
+        <h5><strong><?php echo $product[$i]["name"]?></strong></h5>
+        <h6><?php echo $product[$i]["price"]?></h6>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-4 products-align">
+      <div class="products-align">
+        <img src="<?php echo "img/home_images/".$product[$i + 1]["urlimage"] ?>" alt="home_image">
+        <h5><strong><?php echo $product[$i + 1]["name"]?></strong></h5>
+        <h6><?php echo $product[$i + 1]["price"]?></h6>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-4 products-align">
+      <div class="products-align">
+        <img src="<?php echo "img/home_images/".$product[$i + 2]["urlimage"] ?>" alt="home_image">
+        <h5><strong><?php echo $product[$i + 2]["name"]?></strong></h5>
+        <h6><?php echo $product[$i + 2]["price"]?></h6>
+      </div>
+    </div>
+  </div>
+  <?php endfor ;?>
