5b0293f26cd31ab57ec82a60cf64289eca762521
Giovanni <giovanni.messina3@studio.unibo.it>
2020-12-28 18:16:32 +0100

    Implemented creation of new product (with personal dir) and update of product

--- utils/addProduct.php
+++ utils/addProduct.php
@@ -1,28 +1,33 @@
 <?php
-require_once("../boot.php");
+require_once '../boot.php';
 
-//UPLOAD image
-//TODO mkdir permission denied. Resolve!!!
-    //create dir of the user for upload and profile photo
-    echo UPLOAD_DIR.$_SESSION["id"];
-    if(isset($_FILES["fileToUpload"]["name"])){
-        if(!is_dir(UPLOAD_DIR.$_SESSION["id"])){
-            mkdir(UPLOAD_DIR.$_SESSION["id"], 0777, true);
-            echo "CREATE";
-        }
-        mkdir("/unibowebsite/img/UPLOAD/8/", 0777, true);
+if(isset($_POST["update"])){
+  $dbh -> updateProduct((int)$_POST["idtext"], $_POST["nametext"], $_POST["descriptiontext"], (float)$_POST["pricetext"], (int)$_POST["stocktext"]);
+  header("Location:/unibowebsite/profile.php");
 
-$target_dir = UPLOAD_DIR.$_SESSION["id"]."/";
-$target_file = $target_dir. basename($_FILES["fileToUpload"]["name"]);
-//echo $target_file;
+}
 
+else{
 
+  $upload_dir = "../img/UPLOAD/";
+  //UPLOAD image
+  //TODO mkdir permission denied. Resolve!!!
+  //create dir of the user if it's the first upload for upload and profile photo
+  if(isset($_FILES["fileToUpload"]["name"])){
+    if(!is_dir($upload_dir.$_SESSION["id"])){
+      mkdir($upload_dir.$_SESSION["id"], 0777, true);
     }
 
-//$dbh -> createProduct($_POST["nameProduct"], $_POST["descriptionProduct"], $_POST["PriceProduct"],
-    //                        $_POST["stockProduct"], $_SESSION["id"], $_POST["nameProduct"], $urlimage);
+    $target_dir = $upload_dir.$_SESSION["id"]."/";
+    $target_file = $target_dir. basename($_FILES["fileToUpload"]["name"]);
+    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+
+  }
+
+  $dbh -> createProduct($_POST["nameProduct"], $_POST["descriptionProduct"], (float)$_POST["priceProduct"],
+  (int)$_POST["stockProduct"], $_SESSION["id"], (int)$_POST["categoryProduct"], $_FILES["fileToUpload"]["name"]);
 
 
-
-
- ?>
+  header("Location:/unibowebsite/profile.php?order");
+}
+?>
