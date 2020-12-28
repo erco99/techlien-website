14032360906c636c0c1071061b413ebb52924329
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2020-12-28 17:55:25 +0100

    Add code to shop.php

--- shop.php
+++ shop.php
@@ -2,6 +2,13 @@
 require_once 'boot.php';
 
 $templateParams["file"] = TEMPLATE_DIR."template_shop.php";
+$templateParams["macrocategories"] = $dbh->getMacrocategories();
+
+if(isset($_GET["macrocategoryid"]) && isset($_GET["categoryid"])){
+    $product = $dbh->getProductByCat($_GET["macrocategoryid"], $_GET["categoryid"]);
+} else{
+    $product = $dbh->getAllProducts();
+}
 
 require 'base.php'
 ?>
