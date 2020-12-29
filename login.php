fccfcc3ba7a9bd7116bcb0187f8996a4e4ec6349
Giovanni <giovanni.messina3@studio.unibo.it>
2020-12-29 12:04:59 +0100

    Implemented if you are already logged redirect in profile page

--- login.php
+++ login.php
@@ -1,6 +1,12 @@
 <?php
 require_once 'boot.php';
 
+//if it's yet logged
+if(isset($_SESSION["id"])){
+  header("Location:/unibowebsite/profile.php");
+}
+
+
 if(isset($_GET["create_account"])){
   $templateParams["file"] = TEMPLATE_DIR."template_register.php";
 }
