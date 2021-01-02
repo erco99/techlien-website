ccd18323a83dfff571afab9ce73671465a74cdba
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-01-02 11:10:35 +0100

    Fixes

--- base.php
+++ base.php
@@ -179,22 +179,12 @@
             <div class="row hidden-xs footer-sm-style">
                 <div class="row text-center">
                     <h3><strong>Information</strong></h3>
-                    <p>
-                        <a href="#">About us</a>
-                    </p>
-                    <p>
-                        <a href="#">Contact</a>
-                    </p>
-                    <p>
-                        <a href="#">Technical information</a>
-                    </p>
-
+                    <p><a href="#">About us</a></p>
+                    <p><a href="#">Contact</a></p>
+                    <p><a href="#">Technical information</a></p>
                     <hr width="15%">
-
                     <h3><strong>Altro</strong></h3>
-                    <p>
-                        <a href="#">The story</a>
-                    </p>
+                    <p><a href="#">The story</a></p>
                 </div>
             </div>
             <div class="row last-row">
@@ -203,14 +193,14 @@
         </footer>
     </div>
 
-<!-- jQuery and Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <!-- jQuery and Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
-<!-- Personal javascript files-->
-<script src="js/login_functions.js"></script>
-<script src="js/market_functions.js"></script>
-<script src="js/shop_functions.js"></script>
-<script src="js/general_functions.js"></script>
+    <!-- Personal javascript files-->
+    <script src="js/login_functions.js"></script>
+    <script src="js/market_functions.js"></script>
+    <script src="js/shop_functions.js"></script>
+    <script src="js/general_functions.js"></script>
 </body>
 </html>
