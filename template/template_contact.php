262df05b135cf6858ca32d7a2119b1d595af298d
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2021-01-04 14:24:08 +0100

    Fix contact page style problem

--- template/template_contact.php
+++ template/template_contact.php
@@ -1,9 +1,11 @@
 <div class="row" style="padding: 60px 0 100px;">
     <div class="col-xs-8 col-xs-offset-2 text-center">
         <h2><strong>Contact us</strong></h2>
-        <p style="padding-top: 40px; line-height: 60px;">
+        <p style="padding-top: 40px; line-height: 20px; word-break: break-all;">
             francesco.ercolani3@studio.unibo.it
-            giovanni.messina3@studio.unibo.it
+        </p>
+        <p style="padding-top: 30px; word-break: break-all;">
+        giovanni.messina3@studio.unibo.it
         </p>
     </div>
 </div>
